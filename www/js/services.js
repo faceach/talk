a8e30d97198faa26bf89af5309a6d979d6dd4f8f
faceach <faceach@gmail.com>
2015-04-16 20:16:59 +0800

    change

--- www/js/services.js
+++ www/js/services.js
@@ -2,7 +2,7 @@ angular.module('starter.services', [])
 
 .factory('Talk', function() {
 
-  var bingAccessToken = "http%3a%2f%2fschemas.xmlsoap.org%2fws%2f2005%2f05%2fidentity%2fclaims%2fnameidentifier=watermelon2014&http%3a%2f%2fschemas.microsoft.com%2faccesscontrolservice%2f2010%2f07%2fclaims%2fidentityprovider=https%3a%2f%2fdatamarket.accesscontrol.windows.net%2f&Audience=http%3a%2f%2fapi.microsofttranslator.com&ExpiresOn=1429186512&Issuer=https%3a%2f%2fdatamarket.accesscontrol.windows.net%2f&HMACSHA256=aPql5jpW39yCc1AoyXPVFtZRuKBXe5aBY7CYVteGb%2fk%3d";
+  var bingAccessToken = "http%3a%2f%2fschemas.xmlsoap.org%2fws%2f2005%2f05%2fidentity%2fclaims%2fnameidentifier=watermelon2014&http%3a%2f%2fschemas.microsoft.com%2faccesscontrolservice%2f2010%2f07%2fclaims%2fidentityprovider=https%3a%2f%2fdatamarket.accesscontrol.windows.net%2f&Audience=http%3a%2f%2fapi.microsofttranslator.com&ExpiresOn=1429187025&Issuer=https%3a%2f%2fdatamarket.accesscontrol.windows.net%2f&HMACSHA256=vj6uNCKaLf9EQgU1r7UjS5eC3bIN%2fKp3Ctog63tdC%2b8%3d";
 
   function scriptTranslateRequest(token, text) {
     var from = "en",
